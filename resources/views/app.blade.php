7188d5a9c601f754a341b91081d6490db645b859
toxzwaza <toxzprogram@gmail.com>
2026-01-06 13:29:21 +0900

    あんじゅさんGoogleTagManagerのタグ追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,9 +15,22 @@
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
+
+    <!-- Google Tag Manager アンジュさん-->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N7BDKK6S');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="font-sans antialiased">
+    <!-- Google Tag Manager (noscript) アンジュさん-->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7BDKK6S"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     @inertia
 </body>
 
