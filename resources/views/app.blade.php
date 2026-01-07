daa44dfa70799e7780e2c9be1e3481f2cf7afd7e
toxzwaza <toxzprogram@gmail.com>
2026-01-07 12:28:06 +0900

    タグ修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,19 +16,30 @@
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 
-    <!-- Google Tag Manager アンジュさん-->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-N7BDKK6S');</script>
+    <!-- Google Tag Manager アンジュさん -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5NP4QCSB');
+    </script>
     <!-- End Google Tag Manager -->
 </head>
 
 <body class="font-sans antialiased">
-    <!-- Google Tag Manager (noscript) アンジュさん-->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7BDKK6S"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- Google Tag Manager (noscript) アンジュさん -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NP4QCSB"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
     @inertia
